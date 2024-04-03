28fa64b504a10fe042518aa8b991a5a0e6fc13ad
Ronei Simões Calistro <ronei.simoes@gmail.com>
2024-04-03 17:17:07 -0300

    feat: Implementing events_repository

---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,26 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco de dados")
+def test_insert_event():
+    event = {
+        "uuid": "meu-uuid-e-nois",
+        "title": "meu title",
+        "slug": "meu-slug-aqui",
+        "maximum_attendees": 20
+    }
+
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+
+@pytest.mark.skip(reason="Não necessita")
+def test_get_event_by_id():
+    event_id = "meu-uuid-e-nois"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
+    print(response.title)
