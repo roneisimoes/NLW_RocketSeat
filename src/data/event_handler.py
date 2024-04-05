102a56b7187de94fac48de014730048bbc0bb218
Ronei Simões Calistro <ronei.simoes@gmail.com>
2024-04-05 10:33:10 -0300

    feat: Implementing get event rount

---
+++ src/data/event_handler.py
@@ -0,0 +1,39 @@
+import uuid
+from src.models.repository.events_repository import EventsRepository
+from src.http_types.http_request import HttpRequest
+from src.http_types.http_response import HttpResponse
+
+class EventHandler:
+    def __init__(self) -> None:
+        self.__events_repository = EventsRepository()
+
+    def register(self, http_request: HttpRequest) -> HttpResponse:
+        body = http_request.body
+        body["uuid"] = str(uuid.uuid4())
+        self.__events_repository.insert_event(body)
+
+        return HttpResponse(
+            body={ "eventId": body["uuid"] },
+            status_code=200
+        )
+    
+    def find_by_id(self, http_request: HttpRequest) -> HttpResponse:
+        event_id = http_request.param["event_id"]
+        event = self.__events_repository.get_event_by_id(event_id)
+        if not event: raise Exception("Evento não encontado")
+
+        event_attendees_count = self.__events_repository.count_event_attendees(event_id)
+
+        return HttpResponse(
+            body={
+                "event": {
+                    "id": event.id,
+                    "title": event.title,
+                    "detail": event.details,
+                    "slug": event.slug,
+                    "maxumumAttendees": event.maximum_attendees,
+                    "attendeesAmount": event_attendees_count["attendeesAmount"]
+                }
+            },
+            status_code=200
+        )
